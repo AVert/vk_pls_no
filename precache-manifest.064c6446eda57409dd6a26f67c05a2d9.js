716d2fbb628e9cd27188623552fddc6344bfa6c0
skeichy <skeichy@ya.ru>
2019-04-03 22:10:47 +0400

    Updates

---
+++ precache-manifest.064c6446eda57409dd6a26f67c05a2d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1f62ee2ad65bfde3b58",
+    "url": "/vk_pls_no/static/css/main.323583b2.chunk.css"
+  },
+  {
+    "revision": "d1f62ee2ad65bfde3b58",
+    "url": "/vk_pls_no/static/js/main.d1f62ee2.chunk.js"
+  },
+  {
+    "revision": "233492afcadc4c363388",
+    "url": "/vk_pls_no/static/js/runtime~main.233492af.js"
+  },
+  {
+    "revision": "109dae119cdba4e78800",
+    "url": "/vk_pls_no/static/css/2.187d5c10.chunk.css"
+  },
+  {
+    "revision": "109dae119cdba4e78800",
+    "url": "/vk_pls_no/static/js/2.109dae11.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk_pls_no/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "253003fc7f4456be80b9bb369cae271a",
+    "url": "/vk_pls_no/index.html"
+  }
+];
